7ba6d7b6f1c84cea50f54529772b3cf98a903cc6
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-02-10 14:40:39 -0600

    Realizado parte 4, la implementacion de la acelerometro

--- Assets/Script/ShipScript.cs
+++ Assets/Script/ShipScript.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipScript : MonoBehaviour {
+    /*Atributos de los movimientos de la nave*/
+    Rigidbody2D rigidbody;
+    private float speed = 10f;
 
-	// Use this for initialization
-	void Start () {
-		
+    /*Use this for initialization*/
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    /* Update is called once per frame*/
+    void Update () {
+        float movX = Input.acceleration.x;
+        rigidbody.transform.Translate(Vector2.right * speed * movX * Time.deltaTime);
 	}
 }
