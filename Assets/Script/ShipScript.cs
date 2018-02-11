f3621739653e062dbedb5273e27bb697fb0d3438
DESKTOP-VMUPS6B\massa <hmmyfh@gmail.com>
2018-02-11 10:03:25 -0600

    GG

--- Assets/Script/ShipScript.cs
+++ Assets/Script/ShipScript.cs
@@ -7,6 +7,7 @@ public class ShipScript : MonoBehaviour {
     Rigidbody2D rigidbody;
     private float speed = 10f;
     public static bool gameover = false;
+
     /*Use this for initialization*/
     void Start () {
         rigidbody = GetComponent<Rigidbody2D>();
